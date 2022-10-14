d1aa8179017a5c27b0d2f8ec77fc402079336325
Wesley de Souza <wesley_duarte122@hotmail.com>
2022-10-13 22:30:52 -0300

    feat: Desabilita asChild  no storybook

--- src/components/Text.stories.tsx
+++ src/components/Text.stories.tsx
@@ -6,42 +6,45 @@ export default {
   component: Text,
   args: {
     children: "Lorem ipsum.",
-    size: 'md',
+    size: "md",
   },
   argTypes: {
     size: {
-        options: ["sm", "md", "lg"],
-        control: {
-            type: "inline-radio",
-        }
-    }
-  }
+      options: ["sm", "md", "lg"],
+      control: {
+        type: "inline-radio",
+      },
+    },
+  },
 } as Meta<TextProps>;
 
 export const Default: StoryObj<TextProps> = {};
 export const Small: StoryObj<TextProps> = {
-    args: {
-        size: "sm"
-    }
+  args: {
+    size: "sm",
+  },
 };
 export const Large: StoryObj<TextProps> = {
-    args: {
-        size: "lg"
-    }
+  args: {
+    size: "lg",
+  },
 };
 
 export const CustomComponent: StoryObj<TextProps> = {
-    args: {
-        asChild: true,
-        children: (
-            <p>Text with P tag</p>
-        )
+  args: {
+    asChild: true,
+    children: <p>Text with P tag</p>,
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
     },
-    argTypes: {
-        children: {
-            table: {
-                disable: true
-            }
-        }
-    }
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
