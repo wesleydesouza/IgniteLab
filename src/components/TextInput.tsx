2190b1fbef0373177f84ea58d04df3914328009d
Wesley de Souza <wesley_duarte122@hotmail.com>
2022-10-13 23:17:05 -0300

    feat: adiciona component TextInput e stories

---
+++ src/components/TextInput.tsx
@@ -0,0 +1,27 @@
+import { clsx } from "clsx";
+import { InputHTMLAttributes, ReactNode } from "react";
+
+export interface TextInputRootProps{
+  children: ReactNode
+}
+
+export function TextInputRoot(props: TextInputRootProps) {
+  return(
+    <div className="flex items-center gap-3 py-4 px-3 w-full rounded bg-gray-800 focus-within:ring-2 ring-cyan-300">
+      {props.children}
+    </div>
+  )
+}
+
+//function TextInputIcon() {}
+
+export interface TextInputInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+function TextInputInput(props: TextInputInputProps) {
+  return <input className="bg-transparent flex-1 text-100 text-xs placeholder: text-gray-400 outline-none" {...props} />;
+}
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+}
