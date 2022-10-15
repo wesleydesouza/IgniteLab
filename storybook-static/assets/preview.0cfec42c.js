3aea01b7992fd967863bce29387ede5df6c3f59a
Wesley de Souza <wesley_duarte122@hotmail.com>
2022-10-14 21:58:22 -0300

    feat: adiciona na main

---
+++ storybook-static/assets/preview.0cfec42c.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.55ff2b99.js";import"./iframe.dc156488.js";import"./es.map.constructor.447ca08f.js";import"./es.number.to-fixed.fb5ac20b.js";import"./index.66ff466c.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.0cfec42c.js.map
