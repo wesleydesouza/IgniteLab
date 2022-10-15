4c05635531d16d4219b3408cffc05ee6964e706b
Wesley de Souza <wesley_duarte122@hotmail.com>
2022-10-14 21:28:54 -0300

    feat: cria componente TextInput, TextInput.stories

--- src/components/TextInput.stories.tsx
+++ src/components/TextInput.stories.tsx
@@ -1,12 +1,18 @@
 import { Meta, StoryObj } from "@storybook/react";
-import { TextInput, TextInputInputProps, TextInputRootProps} from "./TextInput";
+import { TextInput, TextInputRootProps} from "./TextInput";
+import { Envelope } from "phosphor-react"
 
 export default {
   title: "Components/TextInput",
   component: TextInput.Root,
   args: {
     children: (
-      <TextInput.Input placeholder="Type your e-mail address" />
+      [
+        <TextInput.Icon>
+          <Envelope/>
+        </TextInput.Icon>,
+        <TextInput.Input placeholder="Type your e-mail address" />
+      ]
     )
   },
   argTypes: {
@@ -19,3 +25,9 @@ export default {
 } as Meta<TextInputRootProps>;
 
 export const Default: StoryObj<TextInputRootProps> = {};
+
+export const WithoutIcon: StoryObj<TextInputRootProps> = {
+  args: {
+    children: <TextInput.Input placeholder="Type your e-mail address" />
+  }
+};
